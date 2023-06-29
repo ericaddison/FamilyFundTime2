3e2f24516ad63b5732dd9ded35f1ca6d2b9aadcf
Eric Addison <ericaddison@gmail.com>
2023-06-28 21:59:53 -0700

    Added profile pic

--- app/src/main/java/com/lutortech/familyfundtime/ui/signin/SignIn.kt
+++ app/src/main/java/com/lutortech/familyfundtime/ui/signin/SignIn.kt
@@ -2,9 +2,16 @@ package com.lutortech.familyfundtime.ui.signin
 
 import android.net.Uri
 import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,6 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.FixedScale
+import androidx.compose.ui.unit.Dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.lutortech.familyfundtime.Constants.LOG_TAG
@@ -23,22 +33,26 @@ fun SignIn(signInViewModel: SignInViewModel, modifier: Modifier = Modifier) {
     val user by remember { signInViewModel.user }
     val isSignedIn = user != null
 
-    if (isSignedIn) {
-        Row(
-            modifier = modifier
-        ) {
-            SignOutButton(
-                onClick = { signInViewModel.sendEvent(SignInEvent.USER_EVENT_SIGN_OUT) },
-                modifier = modifier
+    Surface(
+        modifier = modifier
+            .border(width = Dp(1f), color = Color.Cyan)
+            .padding(Dp(5f))
+    ) {
+        if (isSignedIn) {
+            Row{
+                SignOutButton(
+                    onClick = { signInViewModel.sendEvent(SignInEvent.USER_EVENT_SIGN_OUT) },
+                    modifier = modifier
+                )
+                Text(user?.displayName ?: "NO USER", modifier = modifier, color = Color.Cyan)
+                ProfilePic(picUrl = user?.profilePicUrl, modifier = modifier)
+            }
+        } else {
+            SignInButton(
+                { signInViewModel.sendEvent(SignInEvent.USER_EVENT_LAUNCH_SIGN_IN) },
+                modifier.fillMaxWidth()
             )
-            Text(user?.displayName ?: "NO USER", modifier = modifier, color = Color.Cyan)
-            ProfilePic(picUrl = user?.profilePicUrl)
         }
-    } else {
-        SignInButton(
-            { signInViewModel.sendEvent(SignInEvent.USER_EVENT_LAUNCH_SIGN_IN) },
-            modifier
-        )
     }
 
 }
@@ -68,6 +82,7 @@ private fun ProfilePic(picUrl: Uri?, modifier: Modifier = Modifier) {
     GlideImage(
         model = picUrl,
         contentDescription = "Profile Pic",
-        modifier = modifier.clip(CircleShape)
+        modifier = modifier.clip(CircleShape),
+        contentScale = FixedScale(0.1f)
     )
 }
